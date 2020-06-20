315860fe54fea253fbbf247b69a048a9295576f8
DevPopat <popatd@uci.edu>
2020-06-20 11:46:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/flixterpart2/models/Movie.java
@@ -0,0 +1,64 @@
+package com.example.flixterpart2.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+
+
+public class Movie {
+
+    int movieId;
+    String posterPath;
+    String title;
+    String overview;
+    double rating;
+
+    public double getRating() {
+        return rating;
+    }
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public Movie() {
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+
+        for(int i = 0; i<movieJsonArray.length(); i++)
+        {
+            JSONObject movieObject = movieJsonArray.getJSONObject(i);
+            Movie movie = new Movie(movieObject);
+            movies.add(movie);
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
